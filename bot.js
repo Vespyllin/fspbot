ee7caacacc080ea669f7838c1b016c4d88e4c0cd
Michael <Debono>
2020-11-16 16:28:47 +0100

    fixed entry

--- bot.js
+++ bot.js
@@ -20,67 +20,67 @@ let scheduledMessage = new cron.CronJob('0,30 * * * *', () => {
 });
 
 scheduledMessage.start()
- 
 
-client.on('message', async(msg) => {
+
+client.on('message', async (msg) => {
     var args = msg.content.substring(PREFIX.length).split(" ");
 
-    if(msg.author.equals(client.user)) return;
-    if(!msg.content.startsWith(PREFIX) || !msg.content.startsWith('<:JamesPog:')) return;
-    console.log("I'm here")
-    switch(args[0].toLowerCase()) {
-        case "mute":
-            if(!msg.mentions.users.first()) return msg.channel.send("you need to mention somebody!!");
+    if (msg.author.equals(client.user)) return;
+    if (msg.content.startsWith(PREFIX)) {
+        switch (args[0].toLowerCase()) {
+            case "mute":
+                if (!msg.mentions.users.first()) return msg.channel.send("you need to mention somebody!!");
 
-            const voting = new Discord.MessageEmbed()
-                                      .setColor('#42b34d')
-                                      .setFooter('Mute ' + msg.mentions.users.first().tag + ' for 15m?')
-                                      .setImage(msg.mentions.users.first().avatarURL);
-            const role = msg.guild.roles.cache.find(r => r.name === 'Muted');
-            if (!role) return msg.channel.send('No Role was found, please make sure you have a muted role.');
-            const agree = "✅"
-            const disagree ="❌"
-            const sentEmbed = await msg.channel.send(voting);
-            const filter = (reaction, user) => (reaction.emoji.name === agree || reaction.emoji.name === disagree) && !user.bot;
-            await sentEmbed.react(agree);
-            await sentEmbed.react(disagree);
+                const voting = new Discord.MessageEmbed()
+                    .setColor('#42b34d')
+                    .setFooter('Mute ' + msg.mentions.users.first().tag + ' for 15m?')
+                    .setImage(msg.mentions.users.first().avatarURL);
+                const role = msg.guild.roles.cache.find(r => r.name === 'Muted');
+                if (!role) return msg.channel.send('No Role was found, please make sure you have a muted role.');
+                const agree = "✅"
+                const disagree = "❌"
+                const sentEmbed = await msg.channel.send(voting);
+                const filter = (reaction, user) => (reaction.emoji.name === agree || reaction.emoji.name === disagree) && !user.bot;
+                await sentEmbed.react(agree);
+                await sentEmbed.react(disagree);
 
-            const voteStatus = await msg.channel.send('Voting started 30 seconds left');
-            const collected = await sentEmbed.awaitReactions(filter, {time : 30000});
-            const agreed = collected.get(agree) || {count: 1};
-            const disagreed = collected.get(disagree) || {count : 1};
-            const agreed_count = agreed.count;
-            const disagreed_count = disagreed.count;
-            voteStatus.edit('Voting ended with: ' + agreed_count + agree + ' and ' + disagreed_count + disagree);
-            if (agreed.count > disagreed.count) {
-                await msg.guild.member(msg.mentions.users.first()).roles.add(role);
-                if(msg.guild.member(msg.mentions.users.first()).voice.channel)
-                    await msg.guild.member(msg.mentions.users.first()).voice.setMute(true);
-                setInterval(function(){
-                    console.log("unmuting" + msg.mentions.users.first());
-                    if(msg.guild.member(msg.mentions.users.first()).voice.channel){
-                        msg.guild.member(msg.mentions.users.first()).voice.setMute(false);
-                    }
-                    msg.guild.member(msg.mentions.users.first()).roles.remove(role);
-                }, 900000);
-            }   
-            else {
-                msg.channel.send('Mute Voting Failed 🥚')
-            }
-            break;
-        case "jotime":
-            msg.reply("https://www.facebook.com/elizabeth.pace.39")
-            break;
+                const voteStatus = await msg.channel.send('Voting started 30 seconds left');
+                const collected = await sentEmbed.awaitReactions(filter, { time: 30000 });
+                const agreed = collected.get(agree) || { count: 1 };
+                const disagreed = collected.get(disagree) || { count: 1 };
+                const agreed_count = agreed.count;
+                const disagreed_count = disagreed.count;
+                voteStatus.edit('Voting ended with: ' + agreed_count + agree + ' and ' + disagreed_count + disagree);
+                if (agreed.count > disagreed.count) {
+                    await msg.guild.member(msg.mentions.users.first()).roles.add(role);
+                    if (msg.guild.member(msg.mentions.users.first()).voice.channel)
+                        await msg.guild.member(msg.mentions.users.first()).voice.setMute(true);
+                    setInterval(function () {
+                        console.log("unmuting" + msg.mentions.users.first());
+                        if (msg.guild.member(msg.mentions.users.first()).voice.channel) {
+                            msg.guild.member(msg.mentions.users.first()).voice.setMute(false);
+                        }
+                        msg.guild.member(msg.mentions.users.first()).roles.remove(role);
+                    }, 900000);
+                }
+                else {
+                    msg.channel.send('Mute Voting Failed 🥚')
+                }
+                break;
+            case "jotime":
+                msg.reply("https://www.facebook.com/elizabeth.pace.39")
+                break;
+        }
     }
-    if (msg.content.startsWith('<:JamesPog:')){
+    else if (msg.content.startsWith('<:JamesPog:')) {
         console.log("Hey")
-        let connection =  await msg.member.voiceChannel.join()
+        let connection = await msg.member.voiceChannel.join()
         let stream = ytdl(JBSVIDEOS[Math.floor(Math.random() * JBSVIDEOS.length)])
         let dispatcher = await connection.playStream(stream)
-        dispatcher.on('end', _end => {MessageChannel.member.voiceChannel.leave()});
+        dispatcher.on('end', _end => { MessageChannel.member.voiceChannel.leave() });
     }
 });
- 
+
 
 // THIS  MUST  BE  THIS  WAY
 console.log("Logging in...")
