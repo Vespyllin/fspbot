9e64ba42a253a75a6a200a614d896cbe04b70659
Michael <Debono>
2020-11-16 16:31:35 +0100

    cache fix

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on('ready', () => {
 });
 
 let scheduledMessage = new cron.CronJob('0,30 * * * *', () => {
-    let channel = client.channels.get('404664933452873732');
+    let channel = client.channels.cache.get('404664933452873732');
     channel.send(SHAUNPACESWEARWORDS[Math.floor(Math.random() * SHAUNPACESWEARWORDS.length)]);
 });
 
