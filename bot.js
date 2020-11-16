b1483c2750dc28ab40d615cc85ba8e2b9870262a
Michael <Debono>
2020-11-16 12:49:01 +0100

    mute function

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js');
+const cron = require('cron');
 
 const client = new Discord.Client();
-
- 
+const prefix = "🥚"
+const SHAUNPACE = "450287369766305822"
 
 client.on('ready', () => {
 
@@ -12,16 +13,72 @@ client.on('ready', () => {
 
  
 
-client.on('message', message => {
+client.on('message', async(message) => {
+    var args = msg.content.substring(PREFIX.length).split(" ");
 
-    if (message.content === 'ping') {
+    function wait(ms){
+        var start = new Date().getTime();
+        var end = start;
+        while(end < start + ms){
+            end = new Date().getTime();
+        }
+    }
 
-       message.reply('pong');
+    if(msg.author.equals(client.user)) return;
+    if(!msg.content.startsWith(PREFIX)) return;
+    switch(args[0].toLowerCase()) {
+        case "mute":
+            const role = msg.guild.roles.find(r => r.name === 'Muted');
+            if(msg.mentions.members.first().roles.has(role.id)) return
 
-       }
+            if(!msg.mentions.users.first()) return msg.channel.send("you need to mention somebody!!");
 
+            const voting = new Discord.RichEmbed()
+                                      .setColor('#42b34d')
+                                      .setFooter('Mute ' + msg.mentions.users.first().tag + ' for 15m?')
+                                      .setImage(msg.mentions.users.first().avatarURL);
+            if (!role) return msg.channel.send('No Role was found, please make sure you have a muted role.')
+            const agree = "✅"
+            const disagree ="❌"
+
+            const sentEmbed = await msg.channel.send(voting);
+            const filter = (reaction, user) => (reaction.emoji.name === agree || reaction.emoji.name === disagree) && !user.bot;
+            await sentEmbed.react(agree);
+            await sentEmbed.react(disagree);
+
+            const voteStatus = await msg.channel.send('Voting started 30 seconds left');
+            const collected = await sentEmbed.awaitReactions(filter, {time : 5000});
+            const agreed = collected.get(agree) || {count: 1};
+            const disagreed = collected.get(disagree) || {count : 1};
+            const agreed_count = agreed.count - 1;
+            const disagreed_count = disagreed.count - 1;
+            voteStatus.edit('Voting ended with: ' + agreed_count + agree + ' and ' + disagreed_count + disagree);
+            if (agreed.count > disagreed.count) {
+                await msg.guild.member(msg.mentions.users.first()).addRole(role)
+                await wait(900000);
+                await msg.guild.member(msg.mentions.users.first()).removeRole(role)
+            }   
+            else {
+                msg.channel.send('Mute Voting Failed 🥚')
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 });
-
  
 
 // THIS  MUST  BE  THIS  WAY
